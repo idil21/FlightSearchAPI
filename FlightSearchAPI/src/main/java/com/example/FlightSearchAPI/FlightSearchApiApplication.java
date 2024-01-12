6f546f64ccf96e125b2c24881ee5ed41dcb00360
idil21 <102162076+idil21@users.noreply.github.com>
2024-01-12 23:48:39 +0300

    create flightsearch application

---
+++ FlightSearchAPI/src/main/java/com/example/FlightSearchAPI/FlightSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlightSearchAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+@SpringBootApplication
+public class FlightSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSearchApiApplication.class, args);
+	}
+
+}
